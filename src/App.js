7047bd094fb9c654adf64464e483f8059785dfa1
Jerry Pallath <jpallath@nyu.edu>
2018-04-13 22:11:30 -0400

    trying to push to a site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navigation from "./components/navigation";
+import Profile from "./components/profile";
+import Projects from "./components/projects";
+import Links from "./components/links";
+import Contact from "./components/contact";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navigation />
+          <Route exact path="/" render={props => <Profile />} />
+          <Route exact path="/projects" render={props => <Projects />} />
+          <Route exact path="/links" render={props => <Links />} />
+          <Route exact path="/contact" render={props => <Contact />} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
