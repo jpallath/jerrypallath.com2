e4d62a302dc54f6a8c8938a4273e1da01e5b5f26
Jerry Pallath <jpallath@nyu.edu>
2018-05-19 13:48:16 -0400

    tooltips

--- src/components/blogmenu.js
+++ src/components/blogmenu.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import BlogForward from "../components/blogforward";
 import BlogBackward from "../components/blogbackward";
+import BlogMenuItem from "../components/blogmenuitem";
 
 class BlogMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isHovered: "invisble"
+    };
     this.triggerPost = this.triggerPost.bind(this);
     this.changePost = this.changePost.bind(this);
   }
@@ -20,13 +23,11 @@ class BlogMenu extends Component {
   render() {
     let { posts, currentId } = this.props;
     let menu = posts.map(post => (
-      <li
+      <BlogMenuItem
         key={post.id}
-        onClick={() => this.triggerPost(post.id)}
-        className={currentId === post.id ? "active" : null}
-      >
-        {post.title}
-      </li>
+        {...post}
+        triggerPost={this.triggerPost.bind(this)}
+      />
     ));
     return (
       <ul className="blog-menu">
